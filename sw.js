fb49afdd8e965e50f5f12f5393eab5dac2cc6330
Hanif Ali <alihanif016@gmail.com>
2020-11-25 03:31:26 +0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2625ef7f53f5b52c6535.js"
+    "url": "webpack-runtime-c72340b67e57d041862d.js"
   },
   {
     "url": "framework-868c53fff4771b0bde98.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0194ef0abc0fab03fc6ec466f50f2587"
+    "revision": "5da3d9633fd0910643bfad5a32cb5c9b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f135085c99898ba5de814472f2059c73"
+    "revision": "5d42f512ae5e5c1a18b5d764a92a213d"
   },
   {
     "url": "manifest.webmanifest",
